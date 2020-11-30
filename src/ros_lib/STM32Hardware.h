322e037fdaaa85559053af6b6a9daab88d4ba61b
Arun Ravindran <arun.ravindran@intel.com>
2020-11-30 13:46:05 +0200

    Update STM32Hardware.h
    Added support for ethernet based interafce using nucleo-h743ZI

--- src/ros_lib/STM32Hardware.h
+++ src/ros_lib/STM32Hardware.h
@@ -35,11 +35,7 @@
 #ifndef ROS_STM32_HARDWARE_H_
 #define ROS_STM32_HARDWARE_H_
 
-// Change for your device
-//#define STM32F3xx
-#define STM32H7xx
-
-
+#define STM32H7xx  // TODO Move it to build flag
 #ifdef STM32F3xx
 #include "stm32f3xx_hal.h"
 #include "stm32f3xx_hal_uart.h"
@@ -55,81 +51,149 @@
 #ifdef STM32H7xx
 #include "stm32h7xx_hal.h"
 #include "stm32h7xx_hal_uart.h"
+#include "usbd_cdc_if.h"
 #endif /* STM32F7xx */
 
-#ifndef STM32H7xx
-extern UART_HandleTypeDef huart2;
-#else
+#include "main.h"
+
+#ifdef USE_ETHERNET_FOR_ROS
+#include "RosInterface.hpp"
+extern RosInterface* rosInterfacePtr;
+#endif
+
+#ifdef USE_UART_FOR_ROS
 extern UART_HandleTypeDef huart3;
+#endif
+
+#ifdef USE_USB_FOR_ROS
+// Temporary usage in Nucleo board
 #endif
 
 class STM32Hardware {
   protected:
-    UART_HandleTypeDef *huart;
-
-    const static uint16_t rbuflen = 512;
+#ifndef USE_ETHERNET_FOR_ROS
+    const static uint16_t rbuflen = 1024;
+    uint32_t rCurrentSize;
     uint8_t rbuf[rbuflen];
     uint32_t rind;
+
+#ifdef USE_UART_FOR_ROS
+    UART_HandleTypeDef *huart;
     inline uint32_t getRdmaInd(void){ return (rbuflen - __HAL_DMA_GET_COUNTER(huart->hdmarx)) & (rbuflen - 1); }
+#endif
+#ifdef USE_USB_FOR_ROS
+    inline uint32_t getRdmaInd(void){ return (rbuflen -rCurrentSize) & (rbuflen - 1); }
+#endif
 
     const static uint16_t tbuflen = 512;
     uint8_t tbuf[tbuflen];
-    uint16_t twind, tfind;
-  public:
-    STM32Hardware():
-      huart(&huart3), rind(0), twind(0), tfind(0){
-    }
+    uint32_t twind, tfind;
+#endif
 
+  public:
+    STM32Hardware()
+#ifndef USE_ETHERNET_FOR_ROS
+#ifdef USE_UART_FOR_ROS
+     : huart(&huart3),
+#endif
+	  rCurrentSize(0), rind(0), twind(0), tfind(0)
+#endif
+    { }
+
+#ifdef USE_UART_FOR_ROS
     STM32Hardware(UART_HandleTypeDef *huart_):
       huart(huart_), rind(0), twind(0), tfind(0){
+    	rCurrentSize = 0;
     }
-  
+#endif
+
     void init(){
-      reset_rbuf();
+
+#ifndef USE_ETHERNET_FOR_ROS
+        reset_rbuf();
+#endif
+
     }
 
     void reset_rbuf(void){
-      HAL_UART_Receive_DMA(huart, rbuf, rbuflen);
+#ifndef USE_ETHERNET_FOR_ROS
+
+    	memset(tbuf, 0x00, tbuflen);
+#ifdef USE_UART_FOR_ROS
+        HAL_UART_Receive_DMA(huart, rbuf, rbuflen);
+#endif
+        rCurrentSize =0;
+#endif
     }
 
     int read(){
-      int c = -1;
-      if(rind != getRdmaInd()){
-        c = rbuf[rind++];
-        rind &= rbuflen - 1;
-      }
-      return c;
+#ifndef USE_ETHERNET_FOR_ROS
+        int c = -1;
+        if(rind != getRdmaInd()){
+          c = rbuf[rind++];
+          rind &= rbuflen - 1;
+        }
+        return c;
+#else
+        //return getRosInterface()->read();
+        return getRosInterface()->getRxData();
+#endif
+
     }
 
     void flush(void){
+
+#ifndef USE_ETHERNET_FOR_ROS
       static bool mutex = false;
+
+#ifdef USE_UART_FOR_ROS
       if((huart->gState == HAL_UART_STATE_READY) && !mutex){
-        mutex = true;
+#else
+        if(!mutex){
+#endif
+           mutex = true;
 
         if(twind != tfind){
           uint16_t len = tfind < twind ? twind - tfind : tbuflen - tfind;
+#ifdef USE_USB_FOR_ROS
+          CDC_Transmit_FS(&(tbuf[tfind]), len);
+#endif
+
+#ifdef USE_UART_FOR_ROS
           HAL_UART_Transmit_DMA(huart, &(tbuf[tfind]), len);
+#endif
           tfind = (tfind + len) & (tbuflen - 1);
         }
         mutex = false;
       }
+#endif
     }
 
     void write(uint8_t* data, int length){
-      int n = length;
+
+#ifdef USE_ETHERNET_FOR_ROS
+    	//getRosInterface()->write(data, length);
+    	if (!getRosInterface()->SendData(data, length) )
+    	{
+    		// FAiled to send data log it
+    	}
+#else
+      uint32_t n = length;
       n = n <= tbuflen ? n : tbuflen;
 
-      int n_tail = n <= tbuflen - twind ? n : tbuflen - twind;
+      uint32_t n_tail = n <= tbuflen - twind ? n : tbuflen - twind;
       memcpy(&(tbuf[twind]), data, n_tail);
       twind = (twind + n) & (tbuflen - 1);
 
       if(n != n_tail){
         memcpy(tbuf, &(data[n_tail]), n - n_tail);
       }
+
       flush();
+#endif
     }
 
-    unsigned long time(){ return HAL_GetTick();; }
+    unsigned long time(){ return xTaskGetTickCount(); }
 
   protected:
 };
